1c0560a42453451148e79726fcfad9774ba1e48d
Devendra Parmar <23030501043@darshan.ac.in>
2024-11-15 18:50:25 +0530

    Connect the app With MongoDB and implement the CRUD operation with REST API

--- journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,17 +1,27 @@
 package net.engineeringdigest.journalApp.entity;
 
+import java.time.LocalDateTime;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "journal_entries")
 public class JournalEntry { // This is POJO (Plain old java objects)
-    private long id;
+    @Id
+    private ObjectId id;
 
     private String title;
 
     private String content;
 
-    public long getId() {
+    private LocalDateTime date;
+
+    public ObjectId getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(ObjectId id) {
         this.id = id;
     }
 
@@ -29,5 +39,13 @@ public class JournalEntry { // This is POJO (Plain old java objects)
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
     }
 }
