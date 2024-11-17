c9dc5eb675cb1b9a040360b76dbb57e742b3bb9a
Devendra Parmar <23030501043@darshan.ac.in>
2024-11-17 18:52:41 +0530

    I add the project lombok library that automatically plugs the getters, setter, args/noargs constructor in to your java .class file.

--- journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -6,8 +6,16 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Document(collection = "journal_entries")
-public class JournalEntry { // This is POJO (Plain old java objects)
+@Getter
+@Setter
+public class JournalEntry { // * This is POJO (Plain old java objects)
+
+    // ! This is Simple Entity Class for the given fields
+
     @Id
     private ObjectId id;
 
@@ -17,35 +25,35 @@ public class JournalEntry { // This is POJO (Plain old java objects)
 
     private LocalDateTime date;
 
-    public ObjectId getId() {
-        return id;
-    }
+    // public ObjectId getId() {
+    // return id;
+    // }
 
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
+    // public void setId(ObjectId id) {
+    // this.id = id;
+    // }
 
-    public String getTitle() {
-        return title;
-    }
+    // public String getTitle() {
+    // return title;
+    // }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    // public void setTitle(String title) {
+    // this.title = title;
+    // }
 
-    public String getContent() {
-        return content;
-    }
+    // public String getContent() {
+    // return content;
+    // }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+    // public void setContent(String content) {
+    // this.content = content;
+    // }
 
-    public LocalDateTime getDate() {
-        return date;
-    }
+    // public LocalDateTime getDate() {
+    // return date;
+    // }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
+    // public void setDate(LocalDateTime date) {
+    // this.date = date;
+    // }
 }
