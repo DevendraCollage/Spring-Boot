7493e0472b0d136ea80f985eb23b287337362193
Devendra Parmar <23030501043@darshan.ac.in>
2024-11-20 20:37:10 +0530

    Implement the transactional method to rollback the entry if any error has occurred

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -3,6 +3,8 @@ package net.engineeringdigest.journalApp.service;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
@@ -16,11 +18,17 @@ public class JournalEntryService {
     @Autowired
     private UserEntryService userEntryService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String username) {
-        User user = userEntryService.findByUsername(username);
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userEntryService.saveEntry(user);
+        try { // Using for rollback the entry
+            User user = userEntryService.findByUsername(username);
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userEntryService.saveEntry(user);
+        } catch (Exception e) {
+            // TODO: handle exception
+            throw new RuntimeException("An error occurred while saving the entry.", e);
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry) {
